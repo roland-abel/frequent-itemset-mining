a48d760eafc479d420fbc650ea1446d00f4f2ab4
Roland Abel <roland.abel@live.de>
2024-09-21 11:31:17 +0200

    Moved benchmarks tests into separated folder

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -26,7 +26,7 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-#include <benchmark/benchmark.h>
+#include "benchmark/benchmark.h"
 #include <vector>
 #include <algorithm>
 #include <random>
